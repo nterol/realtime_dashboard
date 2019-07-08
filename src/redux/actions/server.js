8b3ee6132afb4287f956c599e44c42e019dc9051
Nicolas Terol <nterol@student.42.fr>
2019-07-08 16:13:45 +0200

    tweaking

--- src/redux/actions/server.js
+++
@@ -1,6 +0,0 @@
-import channelStatus from "../types/channelStatus";
-
-export const startServer = () => ({ type: channelStatus.start });
-export const stopServer = () => ({
-  type: channelStatus.stop
-});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,5 @@
+// cannot make it work in TS at the moment
+
 import { configure } from "enzyme";
 
 import Adapter from "enzyme-adapter-react-16";
