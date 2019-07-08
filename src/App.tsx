d45a9c0c748c2865dca3c2bf012900d127de9774
Nicolas Terol <nterol@student.42.fr>
2019-07-08 16:50:37 +0200

    💅👍

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import Navigation from "./pages/Navigation";
+import { GlobalStyle } from "./components/layout";
 
 function RawApp() {
-  return <Navigation />;
+  return (
+    <>
+      <GlobalStyle />
+      <Navigation />
+    </>
+  );
 }
 
 const App = () => (
